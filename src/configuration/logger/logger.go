ff9bd06d918b2cab9fc60b1508818cac26e46502
lucaskeley <lucaskeley01@gmail.com>
2025-09-09 17:57:23 -0300

    Configuração Inicial de Logger

---
+++ src/configuration/logger/logger.go
@@ -0,0 +1,40 @@
+package logger
+
+import (
+	"os"
+	"strings"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var (
+	log *zap.Logger
+)
+
+func init() {
+
+	logConfig := zap.Config{
+		OutputPaths: []string{getOutputLogs()},
+		Level:       zap.NewAtomicLevelAt(zap.InfoLevel),
+		Encoding:    "json",
+		EncoderConfig: zapcore.EncoderConfig{
+			LevelKey:     "level",
+			TimeKey:      "time",
+			MessageKey:   "message",
+			EncodeTime:   zapcore.ISO8601TimeEncoder,
+			EncodeLevel:  zapcore.LowercaseLevelEncoder,
+			EncodeCaller: zapcore.ShortCallerEncoder,
+		},
+	}
+	log, _ = logConfig.Build()
+
+}
+
+func getOutputLogs() string {
+	output := strings.ToLower(strings.TrimSpace(os.Getenv("LOG_OUTPUT")))
+	if output == "" {
+		return "stdouy"
+	}
+	return output
+}
