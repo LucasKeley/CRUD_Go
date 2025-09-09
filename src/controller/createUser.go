f5e39bf9fc632a61ee9c115aa92462da51ccdca8
lucaskeley <lucaskeley01@gmail.com>
2025-09-09 17:07:06 -0300

    validação de usuário

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,9 +1,7 @@
 package controller
 
 import (
-	"fmt"
-
-	"github.com/LucasKeley/CRUD_Go/src/configuration/rest_err"
+	"github.com/LucasKeley/CRUD_Go/src/configuration/validation"
 	"github.com/LucasKeley/CRUD_Go/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
@@ -11,7 +9,7 @@ import (
 func CreateUser(c *gin.Context) {
 	var UserRequest request.UserRequest
 	if err := c.ShouldBindJSON(&UserRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(fmt.Sprintf("There are some incorrent filds, error=%s\n", err.Error()))
+		restErr := validation.ValidateUserError(err)
 
 		c.JSON(restErr.Code, restErr)
 		return
